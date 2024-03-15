c62ebfb3fa05c4dd52e6dab63f82b25f4ba3bbeb
truyennguyentrung <92207990+truyennguyentrung@users.noreply.github.com>
2024-03-15 19:16:47 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+computernet!
+
+I realize the application has come into reality, has been applied a lot. I hope the project will develop more in the future.
+
+
+
+
---
+++ index.js
@@ -0,0 +1,9 @@
+/* Dependencies */
+
+const areObjects = require('are-objects');
+const areArrays = require('are-arrays');
+
+
+
+const sign = (a, b) => a - b
+exports.sign = sign
